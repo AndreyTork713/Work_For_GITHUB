0a7c199283fcb4aea15fafd71846e23bd86b708d
Andrey Kropotin <tork713@gmail.com>
2019-03-22 12:43:18 +0700

    1 изменение

---
+++ Work_For_GITHUB/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_For_GITHUB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World to Git");
+            Console.WriteLine("Проба работы в GitHub");
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Click to continue...");
+            Console.ReadLine();
+        }
+    }
+}
